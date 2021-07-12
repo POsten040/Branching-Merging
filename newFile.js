18b77972d654c0fceb7c04542fc509f95ab0e504
Patrick <posten.coding@gmail.com>
2021-07-12 13:39:22 -0700

    finished

--- newFile.js
+++ newFile.js
@@ -1,3 +1,11 @@
 function coolfunc(params, sums) {
   console.log(`${params} is ${sum}`)
 }
+//coomment
+//comment
+function name(params) {
+  
+  array.forEach(params => {
+    return element;
+  });
+}
